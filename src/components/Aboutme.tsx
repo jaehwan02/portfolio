8247fe66ceb54add759e8312b427ab337b7e22b5
jaehwan02 <jhrick0129@gmail.com>
2024-12-26 03:02:10 +0900

    shake 애니메이션

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -13,7 +13,7 @@ function Aboutme({ id }: { id: string }) {
 					<i>부산소프트웨어마이스터고등학교(공통과정) 재학중</i>
 					<i>빠른 결정보다는 깊이 있는 생각을 통해 문제를 해결합니다.</i>
 				</div>
-				<img src={bssm} alt="" className="w-[20rem] absolute top-[-1.3rem] right-0" />
+				<img src={bssm} alt="" className="w-[20rem] absolute top-[-1.3rem] right-0 animate-shake" />
 			</div>
 		</SectionLayout>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				shake: {
+					'0%, 25%': { transform: 'rotate(0deg)' },
+					'35%': { transform: 'rotate(-3deg)' },
+					'45%': { transform: 'rotate(3deg)' },
+					'55%, 100%': { transform: 'rotate(0deg)' },
+				},
+			},
+			animation: {
+				shake: 'shake 2.5s ease-in-out infinite',
+			},
+		},
 		fontFamily: {
 			GL: ['GmarketSansLight'],
 			GM: ['GmarketSansMedium'],
